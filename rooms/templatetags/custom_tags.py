6275f6a35a32542f2c8fcd8ba950efc652248705
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-05-15 10:52:08 +0200

    created show numper of ratings in hotel_room page

--- rooms/templatetags/custom_tags.py
+++ rooms/templatetags/custom_tags.py
@@ -8,3 +8,9 @@ def minus_one(num):
     if Room.objects.filter(rating__room=num).exists():
         rate = Room.objects.get(pk=num).total_rating
     return rate
+
+@register.simple_tag
+def user_rating_numper(room,num):
+    room = Room.objects.get(pk=room)
+    rate = room.rating.filter(rating=num)
+    return rate.count()
