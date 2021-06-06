534d487760179c40586f6a5a6365afca5a7f317d
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-06-06 10:33:28 +0200

    tour model in tour app

---
+++ tours/db/receivers.py
@@ -0,0 +1,8 @@
+from django.db.models.signals import pre_save
+from django.template.defaultfilters import slugify
+from ..models import Tour
+
+def tour_slug(sender,instance,*args,**kwargs):
+    instance.slug = slugify(f'{instance.pk}-{instance.title}')
+    
+pre_save.connect(tour_slug, sender=Tour)
--- tours/models.py
+++ tours/models.py
@@ -1,3 +1,26 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from django.utils import timezone
 # Create your models here.
+
+
+class Tour(models.Model):
+    title = models.CharField(max_length = 150)
+    price = models.PositiveIntegerField()
+    country = models.ForeignKey('place', on_delete=models.CASCADE)
+    locality =models.CharField(max_length=50)
+    street = models.CharField(max_length = 150)
+    descriptions = RichTextField()
+    img = models.ImageField(upload_to='rooms/')
+    days_number = models.PositiveIntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(default=timezone.now)
+    slug = models.SlugField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+    
+    def __str__(self):
+        return self.title
+    
+class place(models.Model):
+    place = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
