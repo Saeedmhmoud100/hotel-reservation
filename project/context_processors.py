b30fcbe952c9e4eb3694ce6d411b57278ca1ea77
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-06-07 21:53:53 +0200

    Solve problem of the navbar links in the website pages

--- project/context_processors.py
+++ project/context_processors.py
@@ -1,19 +1,7 @@
 
 def nav_links(request):
-    home,about,room,tour,blog,content = '','','','','',''
-    if request.path == '/': home='active'
-    elif 'about' in request.path: about='active'
-    elif 'hotel' in request.path: room='active'
-    elif 'tour' in request.path: tour='active'
-    elif 'blog' in request.path: blog='active'
-    elif 'content' in request.path: content='active'
+    urlpath = request.path
     cxt = {
-        'home':home,
-        'about':about,
-        'room':room,
-        'tour':tour,
-        'blog':blog,
-        'content':content,
-        
+        'urlpath':urlpath,
     }
     return cxt
