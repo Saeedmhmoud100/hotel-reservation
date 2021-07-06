db22f130863e94e0dd0b8ce4b76a019e3fafd7c6
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-06 12:03:55 +0200

    created contact and FAQ models in about app

--- about/admin.py
+++ about/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import About,FAQ
 
 # Register your models here.
+
+
+@admin.register(About)
+class AboutAdmin(admin.ModelAdmin):
+    class Meta:
+        model = About
+        fields = '__all__'
+
+
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    class Meta:
+        model = FAQ
+        fields = '__all__'
--- about/models.py
+++ about/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class About(models.Model):
+    what_we_do = models.TextField(max_length=1000)
+    our_mission = models.TextField(max_length=1000)
+    our_goal = models.TextField(max_length=1000)
+    image = models.ImageField(upload_to='about/')
+    why_choose_us =  models.TextField(max_length=1000)
+
+    class Meta:
+        verbose_name = ("About")
+        verbose_name_plural = ("About")
+
+    def __str__(self):
+        return str(self.id)
+
+class FAQ(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(max_length=1000)
+    
+
+    class Meta:
+        verbose_name = ("FAQ")
+        verbose_name_plural = ("FAQ")
+
+    def __str__(self):
+        return self.title
