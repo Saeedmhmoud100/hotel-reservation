4425ec22b4d98214697ac79e13bc9a83c9193cdb
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-06 12:29:17 +0200

    created about section in about page

--- about/views.py
+++ about/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView
-from about.models import FAQ
+from about.models import About, FAQ
 
 # Create your views here.
 
 class Aboutview(ListView):
     model = FAQ
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["about"] = About.objects.last() 
+        return context
     
 def contact(request):
     return render(request,'about/contact.html')
