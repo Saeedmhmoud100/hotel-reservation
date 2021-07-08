5c3ec647f5780f0298090b90378f47871dc1b469
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-08 12:09:12 +0200

    starting in register page and credated a register page design in accounts app

--- project/urls.py
+++ project/urls.py
@@ -19,6 +19,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('accounts/',include('accounts.urls', namespace='accounts')),
     path('admin/', admin.site.urls),
     path('',include('main.urls', namespace='main')),
     path('hotels/',include('rooms.urls', namespace='rooms')),
