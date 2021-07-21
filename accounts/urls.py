5a043efbfb321587772ad4ebc229a6b83d887fd0
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-21 11:44:08 +0200

    created logout page in accounts app

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
 from . import views
-from .forms import LoginForm
 app_name = 'accounts'
 
 urlpatterns = [
     path('register/',views.UserRegisterView.as_view(),name='register'),
     path('login/',views.UserLoginView.as_view(),name='login'),
+    path('logout/',LogoutView.as_view(template_name='accounts/logout.html'),name='logout'),
     path('profile/',views.profile,name='profile'),
 ]
