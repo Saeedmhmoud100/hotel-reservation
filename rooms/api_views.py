1dfe926bd7af4ee22451dfb1dfc07eb0bf8392ed
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-25 21:13:51 +0200

    added permissions and created filter in Room Rating api in rooms app

--- rooms/api_views.py
+++ rooms/api_views.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from .permissions import IsStaffUser,IsOwnerOrReadOnly
+from .permissions import IsOwnerUserOrReadOnly, IsStaffUser,IsOwnerOrReadOnly
 from .serializers import RoomSerializers,RoomRatingSerializers
 from .models import Room, Room_Rating
 
@@ -13,3 +13,5 @@ class RoomAPIViewSets(viewsets.ModelViewSet):
 class RoomRatingAPIViewSets(viewsets.ModelViewSet):
     queryset=Room_Rating.objects.all()
     serializer_class=RoomRatingSerializers
+    permission_classes=[IsOwnerUserOrReadOnly]
+    filterset_fields=('user','room')
--- rooms/permissions.py
+++ rooms/permissions.py
@@ -13,3 +13,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
             return True
         return obj.owner == request.user
+
+class IsOwnerUserOrReadOnly(IsStaffUser):
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
+            return True
+        return obj.user == request.user
