cb658286642dab96ca24551e84bcd3042a36ff0f
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-25 22:29:02 +0200

    added permissions and created filter in Tour api in tours app

--- tours/api_views.py
+++ tours/api_views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
 from .serializers import TourSerializers
+from .permissions import IsOwnerOrReadOnly, IsStaffUser
 from .models import Tour
 
 
 class TourAPIViewSets(viewsets.ModelViewSet):
     queryset=Tour.objects.filter(active=True)
     serializer_class=TourSerializers
+    permission_classes=[IsStaffUser,IsOwnerOrReadOnly]
+    search_fields = ['$title', '$descriptions']    
