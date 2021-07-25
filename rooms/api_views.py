8fe77f88352c9bd9ad60cef258053f840a800371
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-25 19:48:28 +0200

    added perimmesions in Room api in rooms app

--- rooms/api_views.py
+++ rooms/api_views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
-from .models import Room
+from .permissions import IsStaffUser,IsOwnerOrReadOnly
 from .serializers import RoomSerializers
+from .models import Room
 
 
 class RoomAPIViewSets(viewsets.ModelViewSet):
     queryset=Room.objects.all()
     serializer_class=RoomSerializers
-    
+    permission_classes=[IsStaffUser,IsOwnerOrReadOnly]
