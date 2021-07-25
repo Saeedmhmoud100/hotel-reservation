8f00495699c6b0bcdec558be3ef9979dca5c25de
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-25 22:16:37 +0200

    update Room Reservation api permissions in rooms app

--- rooms/permissions.py
+++ rooms/permissions.py
@@ -14,8 +14,9 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
             return True
         return obj.owner == request.user
 
-class IsOwnerUserOrReadOnly(IsStaffUser):
+class IsOwnerUserOrReadOnly(permissions.IsAuthenticated):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS or request.user.is_superuser:
             return True
-        return obj.user == request.user
+        return  obj.user == request.user
+
