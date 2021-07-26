6f47e7ebb2d64e0c7a44cc0bae7828bb88e7f28d
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-26 11:29:17 +0200

    created Newsletter_Email and Info apis permission in main app

--- main/api_views.py
+++ main/api_views.py
@@ -1,15 +1,21 @@
-from django.db.models.query_utils import Q
-from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from rest_framework import viewsets
+from rest_framework import permissions, viewsets
 from .models import Newsletter_Email,Info
 from .serializers import NewsletterEmailSerializers, InfoSerializers
 
+class ReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        return bool(request.method in permissions.SAFE_METHODS or request.user.is_superuser)
+    def has_object_permission(self, request, view, obj):
+        return bool(request.method in permissions.SAFE_METHODS or request.user.is_superuser)
 
+class CreateOnly(permissions.IsAuthenticated,ReadOnly):
+    pass
 class NewsletterEmailAPIViewSets(viewsets.ModelViewSet):
     queryset=Newsletter_Email.objects.all()
     serializer_class=NewsletterEmailSerializers
+    permission_classes=[CreateOnly]
 
 class InfoAPIViewSets(viewsets.ModelViewSet):
     queryset=Info.objects.all()
     serializer_class=InfoSerializers
+    permission_classes=[ReadOnly]
