799b0d9d36082a3c62faae6c1c01806a44a04ff6
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-29 11:57:00 +0200

    Start creating tasks and add celery settings

--- project/settings.py
+++ project/settings.py
@@ -212,3 +212,12 @@ AUTHENTICATION_BACKENDS = [
 
 LOGIN_URL = 'accounts:login'
 LOGOUT_URL = 'rest_framework:logout'
+
+
+# CELERY STUFF
+CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = TIME_ZONE
