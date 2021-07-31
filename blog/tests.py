d2fcbecb4b76a95d2017d6276014b876aaa27ed9
Saeedmhmoud100 <saeedmhmoud109@gmail.com>
2021-07-31 12:59:15 +0200

    Created urls test in blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,8 @@
-from django.test import TestCase
+from django.test import TestCase, testcases
 from django.contrib.auth import get_user_model
+from django.urls import reverse,resolve
 from .models import Post,Categorie
+from .views import BlogCreateView, BlogDeleteView, BlogDetailView, BlogListView, BlogUpdateView
 # Create your tests here.
 
 User = get_user_model()
@@ -28,3 +30,28 @@ class TestBlogModel(TestCase):
         self.assertEqual(self.post.get_update_url(),'/blog/1-test-post/update/')
         self.assertEqual(self.post.get_delete_url(),'/blog/1-test-post/delete/')
         
+class TestBlogUrls(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser',password='12345678Tests')
+        self.categorie=Categorie.objects.create(title='test categorie')
+        self.post = Post.objects.create(author=self.user,title='test post',categorie=self.categorie,description=('test posts'*50),img='blog/img1.jpg')
+    
+    def test_post_list_urls(self):
+        url = reverse('blog:blog')
+        self.assertEqual(resolve(url).func.view_class,BlogListView)
+        
+    def test_post_detail_urls(self):
+        url = reverse('blog:blog-detail',args=[self.post.slug])
+        self.assertEqual(resolve(url).func.view_class,BlogDetailView)
+    
+    def test_post_create_urls(self):
+        url = reverse('blog:blog-create')
+        self.assertEqual(resolve(url).func.view_class,BlogCreateView)
+    
+    def test_post_update_urls(self):
+        url = reverse('blog:blog-update',args=[self.post.slug])
+        self.assertEqual(resolve(url).func.view_class,BlogUpdateView)
+    
+    def test_post_delete_urls(self):
+        url = reverse('blog:blog-delete',args=[self.post.slug])
+        self.assertEqual(resolve(url).func.view_class,BlogDeleteView)
